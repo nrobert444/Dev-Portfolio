4e290ddf63311cc9bf25af6f3a85cd737c88bcf3
Nicholas Roberto <njr444@gmail.com>
2020-11-24 22:14:53 -0800

    changed go back link to home.

--- src/components/HeaderSub/HeaderSub.jsx
+++ src/components/HeaderSub/HeaderSub.jsx
@@ -8,7 +8,7 @@ const HeaderSub = () => {
     <HeaderInner>
       <Container>
         <NavContainer>
-          <NavItem to='/'>Go Back</NavItem>
+          <NavItem to='/'>Home</NavItem>
         </NavContainer>{' '}
       </Container>
     </HeaderInner>
