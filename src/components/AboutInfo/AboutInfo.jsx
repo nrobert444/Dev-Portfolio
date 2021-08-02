db5eb666ddba47e3af2bc6b26705f9b69cf17173
Nicholas Roberto <njr444@gmail.com>
2021-08-02 11:43:10 -0700

    added new bio info and link to trailhead account

--- src/components/AboutInfo/AboutInfo.jsx
+++ src/components/AboutInfo/AboutInfo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AboutInfoStyles } from './AboutInfo.styles'
+import { AboutInfoStyles, NoStyleA } from './AboutInfo.styles'
 import { Container } from '../Container/Container'
 import { BottomLine, SectionTitle, Lead } from '../Container/Container.style'
 import Fade from 'react-reveal/Fade'
@@ -23,18 +23,23 @@ export const AboutInfo = () => {
                   With over 8 years of experience optimizing business
                   performance in many industries, I have strong analytical
                   skills with an interest in understanding data and utilizing
-                  information to make informed business decisions. 
-                  
-                  In my free time I enjoy learning all that I can about Salesforce and
+                  information to make informed business decisions. In my free
+                  time I enjoy learning all that I can about Salesforce and
                   related technologies to enchance my understanding and share
                   with the community at large. I am also a member of a
                   Salesforce Professional Development Program to learn
                   additional aspects of the Salesforce Ecosystem and network to
-                  build valuable professional relationships.
-                  
-                  Outside of my passion for Salesforce I enjoy spending time with my family,
+                  build valuable professional relationships. Outside of my
+                  passion for Salesforce I enjoy spending time with my family,
                   experimenting with cooking, exercising, and indulging my
                   passion for cinema.
+                  <br></br>
+                  Check out the link below to see some of my Trailhead
+                  achievements and Certifications!
+                  <br></br>
+                  <NoStyleA href='https://trailblazer.me/id/nroberto2'>
+                    My Trailhead Profile
+                  </NoStyleA>
                 </Lead>
               </div>
             </Fade>
--- src/components/AboutInfo/AboutInfo.styles.jsx
+++ src/components/AboutInfo/AboutInfo.styles.jsx
@@ -33,3 +33,15 @@ export const AboutInfoStyles = styled.div`
     }
   }
 `
+export const NoStyleA = styled.a`
+  text-decoration: none;
+  color: inherit;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`
